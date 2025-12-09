4b58f760ac626172597ac1b886bd6bb91c6e4389
ayenisha <astro.ayeni@gmail.com>
2025-12-10 01:58:11 +0530

    Delete src directory

--- src/ui/ConsoleView.cpp
+++
@@ -1,9 +0,0 @@
-#include "ConsoleView.h"
-#include <iostream>
-
-void ConsoleView::render(int step, const MemoryManager& m, const TraceEntry& r) {
-    std::cout << "\nStep " << step << ": P" << r.pid
-              << " accesses page " << r.page << "\n";
-    m.printFrames();
-    m.printPageTables();
-}
